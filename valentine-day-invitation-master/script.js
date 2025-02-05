f794d38f3878ce1403ede6f3440fcb1301825326
RenzoD30 <renzopiero12@outlook.com>
2025-02-05 18:55:14 -0500

    Add files via upload

---
+++ valentine-day-invitation-master/script.js
@@ -0,0 +1,19 @@
+let leftPos = 0
+let bottomPos = 0
+document.getElementById('yessir').addEventListener('click', () => {
+    alert('Muy bien mi amor, asi me gusta, que me hagas caso. Quiero decirte que estoy muy enamorado de ti y se que lo pasaremos de lo mejor este 14. Amo estar contigo. Te amo 🌷🌷🌷')
+})
+document.getElementById('nooo').addEventListener('mouseover', () => {
+    leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
+    bottomPos = Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    document.getElementById('nooo').style.position = "absolute"
+    document.getElementById('nooo').style.left = leftPos + "%"
+    document.getElementById('nooo').style.bottom = bottomPos + "%"
+})
+document.getElementById('nooo').addEventListener('click', () => {
+    leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
+    bottomPos = Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    document.getElementById('nooo').style.position = "absolute"
+    document.getElementById('nooo').style.left = leftPos + "%"
+    document.getElementById('nooo').style.bottom = bottomPos + "%"
+})
